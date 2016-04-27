90917f8a33e03173a2a2204fe1ddc11e8e6097ca
idlesentry <idlesentry@gmail.com>
2016-04-27 16:29:04 -0700

    Added back missing comments and altered 'new feed selection' suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -77,17 +77,27 @@ $(function() {
     });
 
     describe('Initial Entries', function() {
+        /* TODO: Write a test that ensures when the loadFeed
+         * function is called and completes its work, there is at least
+         * a single .entry element within the .feed container.
+         * Remember, loadFeed() is asynchronous so this test will require
+         * the use of Jasmine's beforeEach and asynchronous done() function.
+         */
         beforeEach(function(done){
             loadFeed(0,done);
         });
 
-        it("should make sure that loadFeed has at least one entry", function(done){
+        it('should make sure that loadFeed has at least one entry', function(done){
             expect($('.feed').children().length).not.toBe(0);
             done();
         });
     });
 
     describe('New Feed Selection', function() {
+        /* TODO: Write a test that ensures when a new feed is loaded
+         * by the loadFeed function that the content actually changes.
+         * Remember, loadFeed() is asynchronous.
+         */
         beforeEach(function(done){
             $('.feed').empty()
 
