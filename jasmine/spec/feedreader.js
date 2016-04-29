6cfd98c4fa20a05de8e19e56ef3fa752a9215968
idlesentry <idlesentry@gmail.com>
2016-04-29 11:42:16 -0700

    Added comments in feedreader.js to indicate how tests are made to fail

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -2,17 +2,20 @@ $(function() {
 
     describe('RSS Feeds', function() {
 
+        //made to fail by removing any definition in JSON (i.e. name: ,)
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
+        //made to fail by removing text inside url definition in JSON (i.e. url: '',)
         it('has URL', function() {
             allFeeds.forEach(function(feed) {
                 expect(feed.url).toBeTruthy();
             });
         });
 
+        //made to fail by removing text inside name definition in JSON (i.e. name: '',)
         it('has name', function() {
             allFeeds.forEach(function(feed) {
                 expect(feed.name).toBeTruthy();
@@ -22,10 +25,12 @@ $(function() {
 
     describe('The Menu', function() {
 
+        //made to fail by changing title of 'menu-hidden' class from index.html
         it('has menu hidden by default', function() {
             expect(document.body.classList).toContain('menu-hidden');
         });
 
+        //made to fail by changing title of 'menu-hidden' class in 'menu-icon.on('click') function in app.js
         it('has menu display when clicked', function() {
             $('.menu-icon-link').trigger('click');
             expect(document.body.className).not.toBe('menu-hidden');
@@ -38,6 +43,7 @@ $(function() {
 
     describe('Initial Entries', function() {
 
+        //made to fail by removing '[id]' from 'feedUrl = allFeeds[id].url' in app.js
         beforeEach(function(done) {
             loadFeed(0, done);
         });
@@ -50,6 +56,7 @@ $(function() {
 
     describe('New Feed Selection', function() {
 
+        //made to fail by removing brackets from '{{title}}' and '{{contentSnippet}}' in index.html
         beforeEach(function(done) {
             $('.feed').empty()
 
